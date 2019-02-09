03959c6a2562f0f2573d79f3cbc9da7edb340200
Nash Brancic <nashfft@gmail.com>
2019-02-09 20:08:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[1..140] + "..."
+    word_substituter(tweet)[0..140] + "..."
   else
     tweet
   end
