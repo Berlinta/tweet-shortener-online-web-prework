3780e9f4fd9ede58886ce5892d4f5776f5d24fa3
Nash Brancic <nashfft@gmail.com>
2019-02-09 19:54:28 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,10 +31,10 @@ end
   
 def selective_tweet_shortener(tweet)
   tweet.split(" ").map do |phrase|
-    if phrase.length < 140
+    if phrase.length > 140
       word_substituter(phrase)
       elsif phrase.length <= 140
       phrase
     end
-  end.join
+  end
 end
